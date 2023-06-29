60fbbf6ed1662cce282f90f263315f7170f4f6fb
misterbander <61056749+misterbander@users.noreply.github.com>
2023-06-29 20:36:46 +0800

    Set up lsp_signature.nvim

--- config.lua
+++ config.lua
@@ -27,6 +27,13 @@ lvim.plugins = {
     config = function()
       require("nvim-ts-autotag").setup()
     end
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require "lsp_signature".on_attach()
+    end
   }
 }
 
