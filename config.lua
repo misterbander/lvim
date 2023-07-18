cebeb45bd57528c7e048a893cb2bb84b5a24aeb8
misterbander <61056749+misterbander@users.noreply.github.com>
2023-07-18 22:43:28 +0800

    Set up nvim-dap-go

--- config.lua
+++ config.lua
@@ -34,6 +34,21 @@ lvim.plugins = {
 		config = function()
 			require "lsp_signature".on_attach()
 		end
+	{
+		"leoluz/nvim-dap-go",
+		config = function()
+			require("dap-go").setup({
+				dap_configurations = {
+					{
+						-- Must be "go" or it will be ignored by the plugin
+						type = "go",
+						name = "Attach remote",
+						mode = "remote",
+						request = "attach",
+					}
+				}
+			})
+		end
 	}
 }
 
