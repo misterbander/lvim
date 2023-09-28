ce4ed64a416eaaea55821632aa72c40fd0dc2d03
misterbander <61056749+misterbander@users.noreply.github.com>
2023-09-28 21:49:46 +0800

    Set up nvim-bqf

--- config.lua
+++ config.lua
@@ -74,7 +74,33 @@ lvim.plugins = {
 		config = function()
 			require("rest-nvim").setup()
 		end
-	}
+	},
+	{
+		"kevinhwang91/nvim-bqf",
+		event = { "BufRead", "BufNew" },
+		config = function()
+			require("bqf").setup({
+				auto_enable = true,
+				preview = {
+					win_height = 12,
+					win_vheight = 12,
+					delay_syntax = 80,
+					border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+				},
+				func_map = {
+					vsplit = "",
+					ptogglemode = "z,",
+					stoggleup = "",
+				},
+				filter = {
+					fzf = {
+						action_for = { ["ctrl-s"] = "split" },
+						extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+					},
+				},
+			})
+		end,
+	},
 }
 
 lvim.builtin.treesitter.autotag.enable = true
